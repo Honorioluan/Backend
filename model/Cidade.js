c62acdfec4120618e208974e7ee3ccbdd805601c
Luan <honorioluan@gmail.com>
2021-03-31 20:18:17 -0300

    Elaboraçao dos novos Models

--- model/Cidade.js
+++
@@ -1,9 +0,0 @@
-/*const api = 'https://cep.awesomeapi.com.br'
-
-module.exports = {
-    async index(req,res){
-        let date = await api+req.query.cep
-        return res.json(data)
-    }
-}
-*/
